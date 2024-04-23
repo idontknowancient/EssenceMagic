1ddbd32b72ddfdf903ccab4068aaa8a4bfcf5239
BuildTools <unconfigured@null.spigotmc.org>
2024-04-23 16:25:05 +0800

    Element damage system is finished, but hasn't been tested out.

--- src/main/java/com/idk/essencemagic/elements/ElementDamage.java
+++ src/main/java/com/idk/essencemagic/elements/ElementDamage.java
@@ -8,30 +8,66 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ElementDamage implements Listener {
+
+    //record what is the type of the element of an armor to attacker's weapon
+    private static final Map<Element, Integer> armorElementMap = new HashMap<>();
 
     @EventHandler
     public static void onAttack(EntityDamageByEntityEvent e) {
         if(!(e.getDamager() instanceof LivingEntity attacker)) return;
         if(!(e.getEntity() instanceof LivingEntity entity)) return;
+        armorElementMap.clear();
+
         double magnification = 1L;
+
         if(ItemHandler.isHoldingCustomItem(attacker)) {
             Item itemInMainHand = ItemHandler.getCorrespondingItem(attacker);
-            Element element = itemInMainHand.getElement();
-            ItemStack helmet = entity.getEquipment().getHelmet();
-            if(helmet != null && ItemHandler.isCustomItem(helmet)) {
-                Element elementHelmet = ItemHandler.getCorrespondingItem(helmet).getElement();
-                if(element.getSuppressMap().containsKey(elementHelmet))
-                    magnification *= element.getSuppressMap().get(elementHelmet);
-                if(element.getSuppressedMap().containsKey(elementHelmet))
-                    magnification *= (1 / element.getSuppressedMap().get(elementHelmet));
-            }
-            if(element.getSuppressMap().containsKey(Element.elements.get("none")))
-                magnification = element.getSuppressMap().get(Element.elements.get("none"));
-            e.setDamage(e.getDamage() * magnification);
-            attacker.sendMessage("attack damage x" + magnification + ", " + e.getDamage());
+            magnification = getElementMagnification(itemInMainHand.getElement(), entity, magnification);
         } else {
-            attacker.sendMessage(e.getDamage() * magnification + "");
+            magnification = getElementMagnification(Element.elements.get("none"), entity, magnification);
+        }
+        e.setDamage(e.getDamage() * magnification);
+        attacker.sendMessage("attack damage x" + magnification + ", " + e.getDamage());
+    }
+
+    //itemInMainHand : item in attacker's main hand / entity : entity who gets damaged / magnification : the original magnification
+    public static double getElementMagnification(Element element, LivingEntity entity, double magnification) {
+
+        setSingularArmorElement(entity.getEquipment().getHelmet());
+        setSingularArmorElement(entity.getEquipment().getChestplate());
+        setSingularArmorElement(entity.getEquipment().getLeggings());
+        setSingularArmorElement(entity.getEquipment().getBoots());
+        for(Element e : armorElementMap.keySet()) {
+            if (element.getSuppressMap().containsKey(e)) //suppress elements
+                if (e.equals(Element.elements.get("none")))
+                    magnification *= Math.pow(element.getSuppressMap().get(e), armorElementMap.get(e));
+                else
+                    magnification *= Math.pow(element.getSuppressMap().get(e), 1d / armorElementMap.get(e));
+            else if (element.getSuppressedMap().containsKey(e)) //suppressed elements
+                magnification *= Math.pow(element.getSuppressedMap().get(e), armorElementMap.get(e) / 1.75);
+            else //nothing
+                magnification *= 1;
+        }
+        return magnification;
+    }
+
+    public static void setSingularArmorElement(ItemStack armor) {
+        if(armor != null && ItemHandler.isCustomItem(armor)) {
+            Element armorElement = ItemHandler.getCorrespondingItem(armor).getElement();
+            if (!armorElementMap.containsKey(armorElement))
+                armorElementMap.put(armorElement, 1);
+            else
+                armorElementMap.put(armorElement, armorElementMap.get(armorElement) + 1);
+        } else { //if not wearing a custom armor or wearing nothing
+            Element none = Element.elements.get("none");
+            if(!armorElementMap.containsKey(none))
+                armorElementMap.put(none, 1);
+            else
+                armorElementMap.put(none, armorElementMap.get(none) + 1);
         }
     }
 }
